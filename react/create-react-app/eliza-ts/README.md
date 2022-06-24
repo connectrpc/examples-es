6555063c55ebc569ff1725086e4f3bef0e5efabc
Steve Ayers <sayers@buf.build>
2022-06-24 13:56:45 -0400

    Better docs

--- react/create-react-app/eliza-ts/README.md
+++ react/create-react-app/eliza-ts/README.md
@@ -4,3 +4,18 @@ This project was bootstrapped with create-react-app using the command
 
 `npx create-react-app eliza-ts --template typescript`
 
+
+## Helpful Commands
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `buf generate buf.build/bufbuild/eliza`
+
+Use `buf` to generate the compiled protos via protobuf-es
+
--- react/create-react-app/eliza-ts/src/App.tsx
+++ react/create-react-app/eliza-ts/src/App.tsx
@@ -1,54 +1,51 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import {
-  makePromiseClient,
-  createConnectTransport,
-} from "@bufbuild/connect-web";
-import {ElizaService} from './gen/buf/connect/demo/eliza/v1/eliza_connectweb';
+    makePromiseClient,
+    createConnectTransport,
+} from '@bufbuild/connect-web'
+import { ElizaService } from './gen/buf/connect/demo/eliza/v1/eliza_connectweb'
 
 function App() {
-    const [question, setQuestion] = useState('');
-    const [answer, setAnswer] = useState('');
+    const [question, setQuestion] = useState('')
+    const [answer, setAnswer] = useState('')
 
     // Make the Eliza Service client
-    const client = makePromiseClient(ElizaService, createConnectTransport({
-      baseUrl: "https://demo.connect.build",
-    }));
-  
+    const client = makePromiseClient(
+        ElizaService,
+        createConnectTransport({
+            baseUrl: 'https://demo.connect.build',
+        })
+    )
+
     const send = async (sentence: string) => {
         const response = await client.say({
-            sentence
-        });
+            sentence,
+        })
 
-        setAnswer(response.sentence);
-    };
+        setAnswer(response.sentence)
+    }
 
-    const handleChange = (event) => {
-        setQuestion(event.target.value);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuestion(event.target.value)
     }
 
     const handleSend = () => {
-        send(question);
+        send(question)
     }
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Say something to Eliza.
-                </p>
-                <input type="text" value={question} onChange={handleChange}/>
-                <p>
-                    {answer}
-                </p>
-                <button onClick={handleSend}>
-                    Send
-                </button>
+                <p>Say something to Eliza.</p>
+                <input type="text" value={question} onChange={handleChange} />
+                <p>{answer}</p>
+                <button onClick={handleSend}>Send</button>
             </header>
         </div>
-  );
+    )
 }
 
-export default App;
+export default App
