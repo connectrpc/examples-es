efb6c119679b1a6406087d63e702d00caaafb782
Steve Ayers <sayers@buf.build>
2022-06-24 11:39:33 -0400

    Added a React directory

---
+++ react/create-react-app/buf/src/App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  makePromiseClient,
+  createConnectTransport,
+} from "@bufbuild/connect-web";
+import {ElizaService} from './gen/proto/buf/connect/demo/eliza/v1/eliza_connectweb';
+
+function App() {
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    // Make the Eliza Service client
+    const client = makePromiseClient(ElizaService, createConnectTransport({
+      baseUrl: "https://demo.connect.build",
+    }));
+  
+    const send = async (sentence) => {
+        const response = await client.say({
+            sentence
+        });
+
+        setAnswer(response.sentence);
+    };
+
+    const handleChange = (event) => {
+        setQuestion(event.target.value);
+    }
+
+    const handleSend = () => {
+        send(question);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Say something to Eliza.
+                </p>
+                <input type="text" value={question} onChange={handleChange}/>
+                <p>
+                    {answer}
+                </p>
+                <button onClick={handleSend}>
+                    Send
+                </button>
+            </header>
+        </div>
+  );
+}
+
+export default App;
