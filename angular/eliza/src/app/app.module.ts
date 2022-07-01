6d214947e316dbce6c375fd78b3637b1414a13a2
Sri Krishna Paritala <skrishna@buf.build>
2022-07-01 17:05:58 +0530

    Angular example with API

---
+++ angular/eliza/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { provideClient } from 'src/connect/client.provider'
+import { ConnectModule } from 'src/connect/connect.module'
+import { ElizaService } from 'src/gen/buf/connect/demo/eliza/v1/eliza_connectweb'
+
+import { AppComponent } from './app.component'
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        ConnectModule.forRoot({
+            baseUrl: 'https://demo.connect.build',
+        }),
+    ],
+    providers: [provideClient(ElizaService)],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
