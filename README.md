9c08d141cb02301da66e1bc3c608387f2e0b071e
Steve Ayers <sayers@buf.build>
2022-07-08 16:01:14 -0400

    Added Remix Typescript

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # connect-web-integration
 Connect Web Integration tests the integration of connect-web in various JS frameworks and tooling
 
-## Tested frameworks
+## Tested frameworks and associated technologies
 
 - React
+-- Bundler: Webpack
+-- Package Manager:  npm
+
+- Remix
+-- Bundler: Remix Compiler (ESBuild)
+-- Package Manager:  pnpm
+
 - Svelte
+-- Bundler: Vite
+-- Package Manager:  npm
+
 - Vue
+-- Bundler: Vite
+-- Package Manager:  npm
---
+++ remix/eliza-ts/app/root.tsx
@@ -0,0 +1,42 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import {
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+} from '@remix-run/react'
+import styles from './assets/app.css'
+
+export const meta: MetaFunction = () => ({
+    charset: 'utf-8',
+    title: 'Eliza TS',
+    viewport: 'width=device-width,initial-scale=1',
+})
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles,
+        },
+    ]
+}
+
+export default function App() {
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    )
+}
