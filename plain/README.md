b6e628d2cb10e3fa7a48c400a8c252b4687083e2
Steve Ayers <sayers@buf.build>
2022-08-24 23:27:46 -0400

    Plain JS

---
+++ plain/README.md
@@ -0,0 +1,27 @@
+# Eliza
+
+This project was created from scratch using no frameworks at all.  Its only dependencies are:
+
+- Connect Web
+- Protobuf ES
+- esbuild
+- TypeScript
+
+## Getting Started
+
+### `npm install`
+### `npm start`
+
+Runs the app using esbuild `serve` functionality.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Helpful Commands
+
+### `npm run build`
+
+Build the app using esbuild.
+
+### `npm run buf:generate`
+
+Use `buf` to generate the compiled protos via protobuf-es
+
---
+++ plain/src/index.ts
@@ -0,0 +1,69 @@
+import {
+    createPromiseClient,
+    createConnectTransport,
+} from '@bufbuild/connect-web'
+import { ElizaService } from './gen/buf/connect/demo/eliza/v1/eliza_connectweb.js'
+import { IntroduceRequest } from './gen/buf/connect/demo/eliza/v1/eliza_pb.js'
+
+let introFinished = false;
+
+// Make the Eliza Service client
+const client = createPromiseClient(
+    ElizaService,
+    createConnectTransport({
+        baseUrl: 'https://demo.connect.build',
+    })
+)
+
+// Query for the common elements and cache them.
+const containerEl = document.getElementById("conversation-container") as HTMLDivElement;
+const inputEl = document.getElementById("user-input") as HTMLInputElement;
+
+// Add an event listener to the input so that the user can hit enter and click the Send button
+document.getElementById("user-input")?.addEventListener("keyup", (event) => {
+    event.preventDefault();
+    if (event.key === "Enter") {
+        document.getElementById("send-button")?.click();
+    }
+});
+
+// Adds a node to the DOM representing the conversation with Eliza
+function addNode(text: string, sender: string): void {
+    const respContainerEl = containerEl.appendChild(document.createElement('div'));
+    respContainerEl.className = `${sender}-resp-container`;
+    
+    const respTextEl = respContainerEl.appendChild(document.createElement('p'));
+
+    respTextEl.className = "resp-text";
+    respTextEl.innerText = text;
+}
+
+async function send() {
+    const sentence = inputEl?.value ?? '';
+
+    addNode(sentence, 'user');
+
+    inputEl.value = '';
+
+    if (introFinished) {
+        const response = await client.say({
+            sentence,
+        })
+
+        addNode(response.sentence, 'eliza');
+    } else {
+        const request = new IntroduceRequest({
+            name: sentence,
+        })
+
+        for await (const response of client.introduce(request)) {
+            addNode(response.sentence, 'eliza');
+        }
+
+        introFinished = true;
+    }
+}
+
+export function handleSend() {
+    send();
+}
