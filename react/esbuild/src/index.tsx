8f9fe67c742e10f8e72b6e9ff06bbb1676451356
Steve Ayers <sayers@buf.build>
2022-09-11 18:07:45 -0400

    Remove unused

--- react/esbuild/src/index.tsx
+++ react/esbuild/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
 import App from "./App.js";
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
