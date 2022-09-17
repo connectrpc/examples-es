fb0a94e2c5e8e706a1bfc71dbd37a1d7a4b2a7c4
Steve Ayers <sayers@buf.build>
2022-09-17 12:49:31 -0400

    Updated Svelte example to use Vite

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,8 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-   plugins: [sveltekit()]
-};
+    plugins: [sveltekit()],
+    preview: {
+        port: 3000,
+    },
+}
 
-export default config;
+export default config
