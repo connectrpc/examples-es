67cfaf8718fb0b23ee6cf5261eb25818dfabaefc
Steve Ayers <sayers@buf.build>
2023-03-09 15:22:40 -0500

    Move config to common js file

--- nextjs/jest.config.cjs
+++ nextjs/jest.config.cjs
@@ -1,10 +1,10 @@
-//est.config.js
-const nextJest = require('next/jest');
+// jest.config.js
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
     dir: './',
-});
+})
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
@@ -15,17 +15,17 @@ const customJestConfig = {
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
     transform: {
-      "^.+\\.(ts|tsx)$": [
-        'ts-jest',
-        {
-          useESM: true,
-        },
-      ],
+        '^.+\\.(ts|tsx)$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
     },
     moduleNameMapper: {
-      "(.+)\\.js": "$1",
+        '(.+)\\.js': '$1',
     },
-};
+}
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig)
