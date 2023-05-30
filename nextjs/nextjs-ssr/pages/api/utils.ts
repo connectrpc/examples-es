45e166b33ad6ff3a12f243838bcf08628069aa2f
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-05-30 13:57:55 -0400

    strips down example considerably

--- nextjs/nextjs-ssr/pages/api/utils.ts
+++ nextjs/nextjs-ssr/pages/api/utils.ts
@@ -1,7 +1,3 @@
-import { createGrpcWebTransport } from "@bufbuild/connect-web";
-import { createPromiseClient } from "@bufbuild/connect";
-import type { ServiceType } from "@bufbuild/protobuf";
-
 /** This utility helps to demonstrate that a custom implementation for fetch is actually being called. */
 export const wrapFetch = (
   message: string,
@@ -12,15 +8,4 @@ export const wrapFetch = (
 ) => {
   console.log(message)
   return fetch(url, init)
-}
-
-export const createClient = <T extends ServiceType>(
-  service: T,
-  fetch: typeof globalThis.fetch,
-) => {
-  const transport = createGrpcWebTransport({
-    baseUrl: 'http://localhost:3000/api', // you cannot use a relative path like `/api` here because SSR will break.  SSR requires absolute URLs.
-    fetch: wrapFetch('calling from gRPC-web', fetch),
-  });
-  return createPromiseClient(service, transport);
 }
--- nextjs/nextjs-ssr/pages/index.tsx
+++ nextjs/nextjs-ssr/pages/index.tsx
@@ -1,118 +1,42 @@
-import React, { useState } from 'react'
-import styles from '../styles/Eliza.module.css'
-import { ElizaService } from '../gen/buf/connect/demo/eliza/v1/eliza_connect.js'
-import { IntroduceRequest } from '../gen/buf/connect/demo/eliza/v1/eliza_pb.js'
-import { InferGetServerSidePropsType } from 'next'
-import { createClient, wrapFetch } from './api/utils'
-
-interface Response {
-    text: string
-    sender: 'eliza' | 'user'
-}
+import React from "react";
+import styles from "../styles/Eliza.module.css";
+import { ElizaService } from "../gen/buf/connect/demo/eliza/v1/eliza_connect.js";
+import { InferGetServerSidePropsType } from "next";
+import { wrapFetch } from "./api/utils";
+import { createConnectTransport } from "@bufbuild/connect-web";
+import { createPromiseClient } from "@bufbuild/connect";
 
 export const getServerSideProps = async () => {
-    const client = createClient(ElizaService, wrapFetch('calling from getServerSideProps', fetch));
-    const { sentence } = await client.say({ sentence: "hi (from the server)" });
-    return { props: { sentence } };
+  const transport = createConnectTransport({
+    // note: you cannot use a relative path like `/api` here because SSR will break.  SSR requires absolute URLs.
+    baseUrl: "https://demo.connect.build",
+    fetch: wrapFetch("calling from getServerSideProps", fetch),
+  });
+  const client = createPromiseClient(ElizaService, transport);
+  const request = { sentence: "hi (from the server)" };
+  const response = await client.say(request);
+
+  return { props: { request, response: { sentence: response.sentence } } };
+  //                         ^ note: you can use `response.toJson()` instead, but you will lose all type information.
 };
 
-function App({ sentence }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    const [statement, setStatement] = useState<string>('')
-    const [introFinished, setIntroFinished] = useState<boolean>(false)
-    const [responses, setResponses] = useState<Response[]>([
-        {
-            text: 'What is your name?',
-            sender: 'eliza',
-        },
-    ])
+function App({
+  request,
+  response,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      <header className={styles.appHeader}>
+        <h1 className={styles.headline}>Eliza</h1>
+        <h4 className={styles.subtitle}>Next.js + SSR</h4>
+      </header>
 
-    // Make the Eliza Service client
-    const client = createClient(ElizaService, wrapFetch('calling from the Page component', fetch))
-
-    const send = async (sentence: string) => {
-        setResponses((resp) => [...resp, { text: sentence, sender: 'user' }])
-        setStatement('')
-
-        if (introFinished) {
-            const response = await client.say({
-                sentence,
-            })
-
-            setResponses((resp) => [
-                ...resp,
-                { text: response.sentence, sender: 'eliza' },
-            ])
-        } else {
-            const request = new IntroduceRequest({
-                name: sentence,
-            })
-
-            for await (const response of client.introduce(request)) {
-                setResponses((resp) => [
-                    ...resp,
-                    { text: response.sentence, sender: 'eliza' },
-                ])
-            }
-
-            setIntroFinished(true)
-        }
-    }
-
-    const handleStatementChange = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setStatement(event.target.value)
-    }
-
-    const handleSend = () => {
-        send(statement)
-    }
-
-    const handleKeyPress = (event: any) => {
-        if (event.key === 'Enter') {
-            handleSend()
-        }
-    }
-
-    return (
-        <div>
-            <header className={styles.appHeader}>
-                <h1 className={styles.headline}>Eliza</h1>
-                <h4 className={styles.subtitle}>Next.js + SSR</h4>
-            </header>
-            <div className={styles.ssr}>
-                <h4>Server Rendered Data</h4>
-                <code>{JSON.stringify(sentence, null, 2)}</code>
-            </div>
-
-            <div className={styles.container}>
-                {responses.map((resp, i) => {
-                    return (
-                        <div
-                            key={`resp${i}`}
-                            className={
-                                resp.sender === 'eliza'
-                                    ? styles.elizaRespContainer
-                                    : styles.userRespContainer
-                            }
-                        >
-                            <p className={styles.respText}>{resp.text}</p>
-                        </div>
-                    )
-                })}
-                <div>
-                    <input
-                        type="text"
-                        className={`${styles.textInput} ${styles.statementInput}`}
-                        value={statement}
-                        onChange={handleStatementChange}
-                        onKeyPress={handleKeyPress}
-                    />
-                    <button className={styles.button} onClick={handleSend}>Send</button>
-                </div>
-            </div>
-        </div>
-    )
-};
+      <div className={styles.ssr}>
+        <h4>Server Rendered Data</h4>
+        <pre>{JSON.stringify({ request, response }, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
 
 export default App;
