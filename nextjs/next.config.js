618851542a74927faff75489daf9ddf2b4d37c3e
Timo Stamm <tstamm@buf.build>
2023-06-15 17:48:39 +0200

    Add explanatory comment to next.config.js (#686)

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,11 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Allow the .js extension in import paths when importing TypeScript files.
+  // It is the standard for ECMAScript modules, but not all bundlers have
+  // caught up yet.
+  // Alternatively, add the plugin option `import_extension=none` in buf.gen.yaml.
   webpack: (config) => {
      config.resolve = {
         ...config.resolve,
         extensionAlias: {
-              '.js': ['.ts', '.js'],
+            '.js': ['.ts', '.js'],
         },
      };
 
