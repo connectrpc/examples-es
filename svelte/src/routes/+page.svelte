62b23dbf601f7646beffb08f19e1b4043f69ac06
Timo Stamm <ts@timostamm.de>
2023-08-21 19:25:33 +0200

    Update .svelte files manually

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { createPromiseClient } from "@bufbuild/connect";
-  import { createConnectTransport } from "@bufbuild/connect-web";
+  import { createPromiseClient } from "@connectrpc/connect";
+  import { createConnectTransport } from "@connectrpc/connect-web";
   import { ElizaService } from "../gen/connectrpc/eliza/v1/eliza_connect.js";
   import { IntroduceRequest } from "../gen/connectrpc/eliza/v1/eliza_pb.js";
 
