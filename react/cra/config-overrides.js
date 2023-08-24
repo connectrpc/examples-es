7dbae400f8aa7eb108f0f3188904e3367f7692f8
Steve Ayers <sayers@buf.build>
2023-08-24 17:37:39 -0400

    Adjust overrides

--- react/cra/config-overrides.js
+++ react/cra/config-overrides.js
@@ -1,10 +1,11 @@
-const ResolveTypeScriptPlugin = require('resolve-typescript-plugin')
+const ResolveTypeScriptPlugin = require("resolve-typescript-plugin");
 
 // config-overrides.js
-module.exports = function override(config, env) {
-    config.resolve = {
-        plugins: [new ResolveTypeScriptPlugin()],
-    }
-
-    return config
-}
+module.exports = {
+    webpack: function (config) {
+        config.resolve = {
+            plugins: [new ResolveTypeScriptPlugin()],
+        };
+        return config;
+    },
+};
