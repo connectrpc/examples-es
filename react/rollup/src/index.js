b86ea8df16b90c7678f384d2c38153d8fb3cc343
Steve Ayers <sayers@buf.build>
2023-09-27 09:11:12 -0400

    Type-check all projects (#1000)
    This ensures type-checking for all projects. Some bundlers do not do
    type-checking by default, such as Parcel, Remix, esbuild, and Astro.
    This adds a `tsc --noEmit` to the build steps for those projects per
    their instructions and then fixes any resulting type errors that occur.

--- react/rollup/src/index.js
+++ react/rollup/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import App from "./App";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
