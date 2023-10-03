2958f9115b1c85bf4f32e94a43559776e259ea6a
Steve Ayers <sayers@buf.build>
2023-10-03 10:25:28 -0400

    Webpack

--- dual-package-hazard-example/test.bash
+++ dual-package-hazard-example/test.bash
@@ -4,10 +4,10 @@ npm ci
 
 # you'll need https://github.com/Schniz/fnm
 
-fnm exec --using=v20 node consumer/index.js
-fnm exec --using=v18 node consumer/index.js
-fnm exec --using=v16 node consumer/index.js
-fnm exec --using=v14 node consumer/index.js
-fnm exec --using=v12 node consumer/index.js
+# fnm exec --using=v20 node consumer/index.js
+# fnm exec --using=v18 node consumer/index.js
+# fnm exec --using=v16 node consumer/index.js
+# fnm exec --using=v14 node consumer/index.js
+# fnm exec --using=v12 node consumer/index.js
 
 node parcel/dist/index.js
