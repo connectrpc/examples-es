c75473890475433e56aa877884947c33bc45ec3d
Steve Ayers <sayers@buf.build>
2023-10-04 11:21:07 -0400

    Tests for bundlers

--- dual-package-hazard-example/test.bash
+++ dual-package-hazard-example/test.bash
@@ -4,10 +4,14 @@ npm ci
 
 # you'll need https://github.com/Schniz/fnm
 
-# fnm exec --using=v20 node consumer/index.js
-# fnm exec --using=v18 node consumer/index.js
-# fnm exec --using=v16 node consumer/index.js
-# fnm exec --using=v14 node consumer/index.js
-# fnm exec --using=v12 node consumer/index.js
+fnm exec --using=v20 node consumer/index.js
+fnm exec --using=v18 node consumer/index.js
+fnm exec --using=v16 node consumer/index.js
+fnm exec --using=v14 node consumer/index.js
+fnm exec --using=v12 node consumer/index.js
 
-# node parcel/dist/index.js
+npm run build:parcel && node parcel/dist/index.js
+npm run build:webpack && node webpack/dist/main.js
+npm run build:rollup && node rollup/dist/bundle.js
+npm run build:vite && node vite/dist/bundle.mjs
+npm run build:esbuild && node esbuild/dist/out.js
