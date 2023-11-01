a9fd238a2b141c92f148cdf185a4050bed5878f2
Steve Ayers <sayers@buf.build>
2023-11-01 12:10:00 -0400

    Fix Astro ports

--- astro/tests/index.spec.ts
+++ astro/tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("visits the app root url", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("http://localhost:4321");
 
   const title = page.locator("h1");
   await expect(title).toHaveText("Eliza");
