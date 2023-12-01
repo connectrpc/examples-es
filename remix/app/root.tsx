e8eeaf822d32cc83fbd48d09c3b1358f1b2f8828
Timo Stamm <ts@timostamm.de>
2023-12-01 16:31:12 +0100

    Update dependencies (#1214)

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -1,42 +1,42 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import type { LinksFunction } from "@remix-run/node";
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-} from '@remix-run/react'
-import styles from './assets/app.css'
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "./assets/app.css";
 
-export const meta: MetaFunction = () => ({
-    charset: 'utf-8',
-    title: 'Eliza',
-    viewport: 'width=device-width,initial-scale=1',
-})
-
-export const links: LinksFunction = () => {
-    return [
-        {
-            rel: 'stylesheet',
-            href: styles,
-        },
-    ]
+export function meta() {
+  return [{ title: "Eliza" }];
 }
 
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
+
 export default function App() {
-    return (
-        <html lang="en">
-            <head>
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <Outlet />
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
