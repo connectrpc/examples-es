35edd1da466efe7232c294485d7cf66b03685086
srikrsna <skrishna@buf.build>
2024-07-08 15:35:35 -0400

    react/webpack-cjs
    Signed-off-by: srikrsna <skrishna@buf.build>

--- react/webpack-cjs/src/App.test.tsx
+++ react/webpack-cjs/src/App.test.tsx
@@ -2,15 +2,14 @@ const { expect, test } = require("@jest/globals");
 
 const {createPromiseClient} = require("@connectrpc/connect");
 const {createConnectTransport} = require("@connectrpc/connect-web");
-const { ElizaService } = require('./gen/connectrpc/eliza/v1/eliza_connect.js');
-const { IntroduceRequest } = require('./gen/connectrpc/eliza/v1/eliza_pb.js');
+const { ElizaService, IntroduceRequestSchema } = require('./gen/connectrpc/eliza/v1/eliza_pb.js');
 
 test('imports ElizaService correctly', () => {
     expect(ElizaService).toBeDefined()
 })
 
 test('imports messages correctly', () => {
-    expect(IntroduceRequest).toBeDefined()
+    expect(IntroduceRequestSchema).toBeDefined()
 })
 
 test('creates a promise client', () => {
