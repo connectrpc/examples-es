6b390677b4c4ce92a1439d7d2d2fa37df9104b4e
Ryan Linton <ryanlntn@gmail.com>
2024-07-09 16:07:04 -0600

    fix polyfills using expo-dev-client and platform splitting
    Signed-off-by: Ryan Linton <ryanlntn@gmail.com>

---
+++ react-native/app/polyfills.native.ts
@@ -0,0 +1,20 @@
+import TextEncoder from "react-native-fast-encoder";
+// @ts-expect-error -- missing type declarations
+import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
+// @ts-expect-error -- missing type declarations
+import { fetch, Headers, Request, Response } from "react-native-fetch-api";
+import { ReadableStream } from "web-streams-polyfill";
+
+export function polyfills() {
+  polyfillGlobal("ReadableStream", () => ReadableStream);
+  polyfillGlobal("TextDecoder", () => TextEncoder);
+  polyfillGlobal("TextEncoder", () => TextEncoder);
+  polyfillGlobal("fetch", () => fetch);
+  polyfillGlobal("Headers", () => Headers);
+  polyfillGlobal("Request", () => Request);
+  polyfillGlobal("Response", () => Response);
+  // Polyfill async.Iterator. For some reason, the Babel presets and plugins are not doing the trick.
+  // Code from here: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-3.html#caveats
+  (Symbol as any).asyncIterator =
+    Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+}
--- react-native/app/polyfills.ts
+++ react-native/app/polyfills.ts
@@ -1,20 +1,2 @@
-import TextEncoder from "react-native-fast-encoder"
-import { ReadableStream } from "web-streams-polyfill"
-// @ts-expect-error -- missing type declarations
-import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions"
-// @ts-expect-error -- missing type declarations
-import { fetch, Headers, Request, Response } from "react-native-fetch-api"
-
-export function polyfills() {
-  polyfillGlobal("ReadableStream", () => ReadableStream);
-  polyfillGlobal("TextDecoder", () => TextEncoder);
-  polyfillGlobal("TextEncoder", () => TextEncoder);
-  polyfillGlobal("fetch", () => fetch);
-  polyfillGlobal("Headers", () => Headers);
-  polyfillGlobal("Request", () => Request);
-  polyfillGlobal("Response", () => Response);
-  // Polyfill async.Iterator. For some reason, the Babel presets and plugins are not doing the trick.
-  // Code from here: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-3.html#caveats
-  (Symbol as any).asyncIterator =
-    Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
-}
+// No polyfills needed for web
+export function polyfills() {}
