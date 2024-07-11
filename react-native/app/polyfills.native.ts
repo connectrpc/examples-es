e69d6e4eb214d28e82d2456e3293dcfaa75844d0
Ryan Linton <ryanlntn@gmail.com>
2024-07-11 08:16:11 -0600

    Fix polyfills using expo-dev-client and platform splitting (#1708)
    Fixes the RN polyfill updates by:
    
    1. Updating the app to use `expo-dev-client`. This was required as
    `react-native-fast-encoder` is a native JSI implementation and not
    provided in expo go.
    2. Platform splitting the polyfills. The RN polyfill function is
    unfortunately a deep import and causes us to run into
    [errors](https://github.com/expo/fyi/blob/main/fb-batched-bridge-config-web.md)
    on web unless we completely remove that code from the web bundle.
    
    Demo:
    <img width="1583" alt="Screenshot 2024-07-09 at 4 16 44 PM"
    src="https://github.com/connectrpc/examples-es/assets/1944151/44bbcc4a-031d-44d6-a517-8583a2763298">
    
    ---------
    
    Signed-off-by: Ryan Linton <ryanlntn@gmail.com>

---
+++ react-native/app/polyfills.native.ts
@@ -0,0 +1,26 @@
+import Constants from "expo-constants";
+import TextEncoder from "react-native-fast-encoder";
+// @ts-expect-error -- missing type declarations
+import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
+// @ts-expect-error -- missing type declarations
+import { fetch, Headers, Request, Response } from "react-native-fetch-api";
+import { ReadableStream } from "web-streams-polyfill";
+
+export function polyfills() {
+  const isExpoGo = Constants.appOwnership === "expo";
+  if (isExpoGo) {
+    require("fast-text-encoding");
+  } else {
+    polyfillGlobal("TextDecoder", () => TextEncoder);
+    polyfillGlobal("TextEncoder", () => TextEncoder);
+  }
+  polyfillGlobal("ReadableStream", () => ReadableStream);
+  polyfillGlobal("fetch", () => fetch);
+  polyfillGlobal("Headers", () => Headers);
+  polyfillGlobal("Request", () => Request);
+  polyfillGlobal("Response", () => Response);
+  // Polyfill async.Iterator. For some reason, the Babel presets and plugins are not doing the trick.
+  // Code from here: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-3.html#caveats
+  (Symbol as any).asyncIterator =
+    Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+}
