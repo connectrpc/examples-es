0850bc44717d41cc9c546da9718f47b3a2bb9e8f
Steve Ayers <sayers@buf.build>
2024-07-30 11:07:21 -0400

    NextJS v14 scaffold
    Signed-off-by: Steve Ayers <sayers@buf.build>

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>
