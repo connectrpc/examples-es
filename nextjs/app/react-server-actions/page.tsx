d156c747fe675e67195bbce0ccc3a59b469f0744
Steve Ayers <sayers@buf.build>
2024-09-11 10:40:37 -0400

    Upgrade nextjs example to NextJS 14 (#1718)
    This updates the example in `nextjs` to use Next.js v14. A few minor
    changes to accommodate v14:
    
    - Accessing dynamic data sources such as cookies is not permitted inside
    a cache scope, so the `getMessages` function we use for a fake DB had to
    be refactored to accept a cookie instead of doing the lookup inside: See
    https://nextjs.org/docs/app/api-reference/functions/unstable_cache
    - `serverActions` are no longer experimental and are the default so that
    was removed from the nextjs config file.
    
    ---------
    
    Signed-off-by: Steve Ayers <sayers@buf.build>
    Signed-off-by: Timo Stamm <ts@timostamm.de>
    Co-authored-by: Timo Stamm <ts@timostamm.de>

--- nextjs/app/react-server-actions/page.tsx
+++ nextjs/app/react-server-actions/page.tsx
@@ -1,7 +1,7 @@
-import styles from "../../styles/Eliza.module.css";
+import styles from "@/styles/Eliza.module.css";
 import { revalidateTag, unstable_cache } from "next/cache";
 import { createPromiseClient } from "@connectrpc/connect";
-import { ElizaService } from "../../gen/connectrpc/eliza/v1/eliza_connect";
+import { ElizaService } from "@/gen/connectrpc/eliza/v1/eliza_connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
 import { getMessages, addMessage } from "./fake-db";
 
