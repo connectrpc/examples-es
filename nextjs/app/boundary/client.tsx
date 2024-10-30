4167eab6fb9b97f64412d135c77a0fb3ea1833aa
Timo Stamm <ts@timostamm.de>
2024-10-30 15:15:09 +0100

    V2: Provide more details in Next.js SSR examples (#1970)
    Signed-off-by: Timo Stamm <ts@timostamm.de>

--- nextjs/app/boundary/client.tsx
+++ nextjs/app/boundary/client.tsx
@@ -5,8 +5,20 @@ import { Payload } from "../../gen/payload_pb";
 export default function Client(props: { payload: Payload }) {
   return (
     <div>
-      <h5>Payload&apos;s large number, rendered on the client</h5>
-      <pre>{props.payload.largeNumber.toString()}</pre>
+      <h5>Payload rendered on the client</h5>
+      <pre>largeNumber: {props.payload.largeNumber.toString()} ({type(props.payload.largeNumber)})</pre>
+      <pre>double: {props.payload.double.toString()} ({type(props.payload.double)})</pre>
+      <pre>bytes: {props.payload.bytes.toString()} ({type(props.payload.bytes)})</pre>
     </div>
   );
+}
+
+function type(value: unknown): string {
+  if (value instanceof Uint8Array) {
+    return "Uint8Array";
+  }
+  if (Array.isArray(value)) {
+    return "Array";
+  }
+  return typeof value;
 }
