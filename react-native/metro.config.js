0f1dd47114c4a4c2905692a15c3686b841ec588b
Steve Ayers <sayers@buf.build>
2024-11-05 12:37:27 -0500

    Add docs
    Signed-off-by: Steve Ayers <sayers@buf.build>

--- react-native/metro.config.js
+++ react-native/metro.config.js
@@ -8,6 +8,10 @@ const config = {
     unstable_allowRequireContext: true,
   },
   resolver: {
+    // Connect-ES and Protobuf-ES use package exports
+    // (https://nodejs.org/docs/latest-v12.x/api/packages.html#packages_exports).
+    //
+    // We need to enable support for them in Metro. See https://metrobundler.dev/docs/package-exports/
     unstable_enablePackageExports: true,
   },
 };
