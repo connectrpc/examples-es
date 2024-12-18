68b684a98bf26c1150820482883accfa37f9b5ab
Steve Ayers <sayers@buf.build>
2024-12-18 10:19:49 -0500

    Cleanup
    Signed-off-by: Steve Ayers <sayers@buf.build>

--- angular/src/connect/connect.module.ts
+++ angular/src/connect/connect.module.ts
@@ -1,34 +1,30 @@
 import { inject, InjectionToken, Provider } from "@angular/core";
 import { Interceptor, Transport } from "@connectrpc/connect";
-import { createConnectTransport, createGrpcWebTransport } from "@connectrpc/connect-web";
+import {
+  createConnectTransport,
+  createGrpcWebTransport,
+} from "@connectrpc/connect-web";
 import { DescService } from "@bufbuild/protobuf";
 import { createObservableClient, ObservableClient } from "./observable-client";
 
-export const TRANSPORT = new InjectionToken<Transport>("connect.transport");
+const TRANSPORT = new InjectionToken<Transport>("connect.transport");
 
-export const INTERCEPTORS = new InjectionToken<Interceptor[]>(
-  "connect.interceptors",
-  {
-    factory: () => [],
-  },
-);
+const INTERCEPTORS = new InjectionToken<Interceptor[]>("connect.interceptors", {
+  factory: () => [],
+});
 
-export function createClientToken<T extends DescService>(service: T): InjectionToken<ObservableClient<T>> {
-  return new InjectionToken(
-    `client for ${service.typeName}`,
-    {
-      factory() {
-        return createObservableClient(service, inject(TRANSPORT));
-      }
-    }
-  );
+export function createClientToken<T extends DescService>(
+  service: T,
+): InjectionToken<ObservableClient<T>> {
+  return new InjectionToken(`client for ${service.typeName}`, {
+    factory() {
+      return createObservableClient(service, inject(TRANSPORT));
+    },
+  });
 }
 
 export function provideConnect(
-  options: Omit<
-    Parameters<typeof createConnectTransport>[0],
-    "interceptors"
-  >,
+  options: Omit<Parameters<typeof createConnectTransport>[0], "interceptors">,
 ): Provider[] {
   return [
     {
@@ -44,10 +40,7 @@ export function provideConnect(
 }
 
 export function provideGrpcWeb(
-  options: Omit<
-    Parameters<typeof createGrpcWebTransport>[0],
-    "interceptors"
-  >,
+  options: Omit<Parameters<typeof createGrpcWebTransport>[0], "interceptors">,
 ): Provider[] {
   return [
     {
