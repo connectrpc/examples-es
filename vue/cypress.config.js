271141f9d38d6ad2072af1e9f6d560221918e191
Steve Ayers <sayers@buf.build>
2025-05-01 11:36:31 -0400

    Deps
    Signed-off-by: Steve Ayers <sayers@buf.build>

--- vue/cypress.config.js
+++ vue/cypress.config.js
@@ -26,6 +26,7 @@ const options = {
 
 export default defineConfig({
   e2e: {
+    supportFile: false,
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
     setupNodeEvents(on) {
