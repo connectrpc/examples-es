679157d939b7cce441009d33cb5ef09393f622fb
Steve Ayers <sayers@buf.build>
2025-05-02 04:35:29 -0400

    Update deps for May 2025 (#2755)
    This updates the deps in the repo for the May dependabot run. A few
    notes:
    
    * ESLint has been updated to the flat config for Next.js and Vue.
    * This does not include React Native updates, which are a bit more
    involved and will be done in a separate PR.
    
    ---------
    
    Signed-off-by: Steve Ayers <sayers@buf.build>

---
+++ vue/eslint.config.ts
@@ -0,0 +1,47 @@
+import { globalIgnores } from "eslint/config";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import pluginVue from "eslint-plugin-vue";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import pluginCypress from "eslint-plugin-cypress/flat";
+import pluginChaiFriendly from "eslint-plugin-chai-friendly";
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
+  },
+
+  globalIgnores([
+    "**/dist/**",
+    "**/dist-ssr/**",
+    "**/coverage/**",
+    "cypress/support/",
+  ]),
+
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended,
+
+  {
+    // See https://github.com/cypress-io/eslint-plugin-cypress?tab=readme-ov-file#cypress-and-chai-recommended
+    ...pluginCypress.configs.recommended,
+    ...pluginChaiFriendly.configs.recommendedFlat,
+    files: [
+      "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+      "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
+      "cypress/support/**/*.{js,ts,jsx,tsx}",
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "off", // disable original rule
+      "chai-friendly/no-unused-expressions": "error",
+    },
+  },
+);
