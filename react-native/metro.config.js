081ab619463f344c334784b202547125aa4e35f9
Steve Ayers <sayers@buf.build>
2025-05-02 13:56:03 -0400

    Comments
    Signed-off-by: Steve Ayers <sayers@buf.build>

--- react-native/metro.config.js
+++ react-native/metro.config.js
@@ -5,11 +5,10 @@ const defaultConfig = getDefaultConfig(__dirname);
 
 const config = {
   resolver: {
-    // Connect-ES and Protobuf-ES use package exports
-    // (https://nodejs.org/docs/latest-v12.x/api/packages.html#packages_exports).
-    //
-    // We need to enable support for them in Metro. See https://metrobundler.dev/docs/package-exports/
-    unstable_enablePackageExports: true,
+    // Connect-ES and Protobuf-ES use package exports (https://nodejs.org/docs/latest-v12.x/api/packages.html#packages_exports).
+    // We no longer need `unstable_enablePackageExports: true` because
+    // they are now enabled by default.
+    // See https://expo.dev/changelog/sdk-53#the-packagejsonexports-field-is-now-enabled-by-default-in-metro-bundler
   },
 };
 
